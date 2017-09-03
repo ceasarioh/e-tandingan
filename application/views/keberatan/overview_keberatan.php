a4297ff8dd385e9fa9828986cc065031df3883bc
ceasarioh <ceasario.hadiwinoto@gmail.com>
2017-09-03 23:47:32 +0700

    akhirnya chart bisa keluar

--- application/views/keberatan/overview_keberatan.php
+++ application/views/keberatan/overview_keberatan.php
@@ -15,7 +15,7 @@
           </div>
           <div class="box-body">
             <div class="chart">
-              <canvas id="areaChart" style="height:250px"></canvas>
+              <canvas id="chart1k" style="height:250px"></canvas>
             </div>
           </div>
           <!-- /.box-body -->
@@ -38,56 +38,6 @@
           <div class="box-body">
             <div class="chart">
               <canvas id="lineChart" style="height:250px"></canvas>
-            </div>
-          </div>
-          <!-- /.box-body -->
-        </div>
-        <!-- /.box -->
-      </div>
-      <!-- /.col (RIGHT) -->
-    </div>
-    <!-- /.row -->
-</section>
-<p class="text-center">Keputusan Keberatan</p>
-<section class="content">
-    <div class="row">
-      <div class="col-md-6">
-        <!-- AREA CHART -->
-        <div class="box box-info">
-          <div class="box-header with-border">
-            <h3 class="box-title">Area Chart</h3>
-
-            <div class="box-tools pull-right">
-              <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-              </button>
-              <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-            </div>
-          </div>
-          <div class="box-body">
-            <div class="chart">
-              <canvas id="areaChart"></canvas>
-            </div>
-          </div>
-          <!-- /.box-body -->
-        </div>
-        <!-- /.box -->
-      </div>
-      <!-- /.col (LEFT) -->
-      <div class="col-md-6">
-        <!-- LINE CHART -->
-        <div class="box box-info">
-          <div class="box-header with-border">
-            <h3 class="box-title">Line Chart</h3>
-
-            <div class="box-tools pull-right">
-              <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-              </button>
-              <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-            </div>
-          </div>
-          <div class="box-body">
-            <div class="chart">
-              <canvas id="lineChart"></canvas>
             </div>
           </div>
           <!-- /.box-body -->
